b50e77af2b8f93e2693556d7ccfa26b6f325882b
HimanshiGarg <59911323+HIMANSHIGARG0@users.noreply.github.com>
2022-12-13 03:02:24 +0530

    Add files via upload

---
+++ GROUP 5 - MY TRIP PLANNER/SOURCE CODE/sprintProject/myTrip/include/commonMacros.h
@@ -0,0 +1,6 @@
+#ifndef COMMONMACROS_H
+#define COMMONMACROS_H
+
+#define MAX_SIZE_NAME 50
+
+#endif
---
+++ GROUP 5 - MY TRIP PLANNER/SOURCE CODE/sprintProject/myTrip/include/register.h
@@ -0,0 +1,9 @@
+#ifndef DESTINATIONS_H
+#define DESTINATIONS_H
+
+int addNewUser(char, char, char);
+void displayRegisteredUsers();
+int writeToUsersFile();
+//int displayUsersFileData();
+
+#endif
