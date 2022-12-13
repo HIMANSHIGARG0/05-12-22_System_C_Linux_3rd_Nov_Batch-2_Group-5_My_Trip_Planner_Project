aa26682c6852579618b19ebff10bf39d5d29220b
HimanshiGarg <59911323+HIMANSHIGARG0@users.noreply.github.com>
2022-12-13 13:29:30 +0530

    Delete GROUP 5 - MY TRIP PLANNER/SOURCE CODE/sprintProject/myTrip directory

--- GROUP 5 - MY TRIP PLANNER/SOURCE CODE/sprintProject/myTrip/include/commonMacros.h
+++
@@ -1,6 +0,0 @@
-#ifndef COMMONMACROS_H
-#define COMMONMACROS_H
-
-#define MAX_SIZE_NAME 50
-
-#endif
--- GROUP 5 - MY TRIP PLANNER/SOURCE CODE/sprintProject/myTrip/include/register.h
+++
@@ -1,9 +0,0 @@
-#ifndef DESTINATIONS_H
-#define DESTINATIONS_H
-
-int addNewUser(char, char, char);
-void displayRegisteredUsers();
-int writeToUsersFile();
-//int displayUsersFileData();
-
-#endif
